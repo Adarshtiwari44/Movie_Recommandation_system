c4322b1414698fd97a20bd084dcdbcd730b4de1c
Adarshtiwari44 <167158705+Adarshtiwari44@users.noreply.github.com>
2026-01-26 19:15:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# Load dataset
+df = pd.read_csv("netflix_titles.csv")
+df = df[['title', 'director', 'cast', 'listed_in', 'description']]
+df.fillna('', inplace=True)
+
+# Create metadata
+df['metadata'] = (
+    df['title'] + ' ' +
+    df['director'] + ' ' +
+    df['cast'] + ' ' +
+    df['listed_in'] + ' ' +
+    df['description']
+).str.lower()
+
+# TF-IDF Vectorization
+tfidf = TfidfVectorizer(stop_words='english')
+tfidf_matrix = tfidf.fit_transform(df['metadata'])
+
+# Cosine similarity
+cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+
+# Recommendation function
+def recommandation(title, n=5):
+    #matches work is to make the "title in dataset and the title enter by user" in lower case..
+    #so we can check whether the movie is present in the data or not
+    matches = df[df['title'].str.lower().str.contains(title.lower())]
+    if matches.empty:
+        return []
+    idx = matches.index[0]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:n+1]
+    movie_indices = [i[0] for i in sim_scores]
+    return df.loc[movie_indices, 'title']
+
+# Streamlit UI
+st.title("🎬 Movie Recommendation System")
+st.write("Type a movie name and get similar recommendations")
+
+movie = st.text_input("Enter movie name")
+
+if st.button("Recommend"):
+    if movie:
+        results = recommandation(movie)
+        if len(results) > 0:
+            st.success("Recommended Movies:")
+            for r in results:
+                st.write("👉", r)
+        else:
+            st.error("Movie not found")
+    else:
+        st.warning("Please enter a movie name")
+
